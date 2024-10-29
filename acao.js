eed0644765bcd79f2afa85f1194d4854b269f72e
Your Name <jabes.candido1@gmail.com>
2024-10-29 17:10:12 -0300

    Alualizando as mensagens do cv e colocando interação na foto

--- acao.js
+++ acao.js
@@ -1,1 +1,8 @@
-// Arquivo acao.js: Adicione aqui qualquer funcionalidade JavaScript necessária
+const profilePic = document.querySelector('.profile-pic');
+
+profilePic.addEventListener('click', () => {
+    profilePic.style.transform = 'scale(1.2)'; // Aumenta a foto ao clicar
+    setTimeout(() => {
+        profilePic.style.transform = 'scale(1)'; // Retorna ao tamanho normal após 300ms
+    }, 300); // Tempo em milissegundos
+});
